4094771130d5500040dc856fc34538963425f463
Gordon_Tang <eaglefairassetmanagement@gmail.com>
2020-01-10 12:42:56 +1100

    Version1

---
+++ tic.js
@@ -0,0 +1,259 @@
+//Variable Declaration
+var spanTag = document.createElement('span')
+var listofSubBoxes = document.querySelectorAll('.box > div')
+var startButton = document.querySelector('.startGame')
+var resetHalButton = document.querySelector('.resetHal')
+
+// switchCounter 1 / 0 
+var switchCounter = 0;
+
+//Counter Variable for vaildate winner Draw and how many game
+var counterO = []
+var counterX = []
+const winningCombine = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8] ]
+
+var gameCounter = 0;
+// variable for Draw counter 
+var winner = 0
+
+
+function cleanerFn(){
+    return {
+        elementsIdReset: (collectionOfElements) => {
+            collectionOfElements.forEach( (element) => element.dataset.id = 'reset')        
+
+        }, 
+        emptyChildElements: (collectionOfElements) => {
+            collectionOfElements.forEach( (element) => $(element).empty() )           
+        }
+            
+    }
+
+}
+
+// alert(`Please press START button to start`)
+
+//Clean child element function 
+var cleaner = cleanerFn()
+
+//reset Event, addEventListener will execute function immediately if you pass a variable
+// you must wrap another function outside to not immidately execute
+resetHalButton.addEventListener('click', function() {
+            
+            //Make sure all global variables are clean & grid is clean
+            counterO = []
+            counterX = []
+            cleaner.emptyChildElements(listofSubBoxes)
+            gameCounter = 0
+
+            // Draw counter variable
+            winner = 0
+                        
+            //Make sure all Div ids are empty
+            cleaner.elementsIdReset(listofSubBoxes)
+
+            //Re establish grid event listener
+            listofSubBoxes.forEach ( 
+                function(item){item.addEventListener('click', ox)
+           
+    })
+})
+
+//startGame function  -> 1. Clean the grid, randomly pick player & message who starts
+var startGame = () => {
+        
+        
+        //Make sure all global variables are clean & grid is clean
+        counterO = []
+        counterX = []
+        cleaner.emptyChildElements(listofSubBoxes)
+        gameCounter = 0
+
+         // Draw counter variable
+         winner = 0
+
+        //Make sure all div id to reset
+        cleaner.elementsIdReset(listofSubBoxes)
+        
+        //Re establish grid event listener
+        listofSubBoxes.forEach ( 
+            function(item){item.addEventListener('click', ox)
+                    
+                        })
+        // randomly pick player
+        let ranNum = Math.round((Math.random() * 10) + 0.5);
+        switchCounter = ranNum % 2
+        if(switchCounter === 0){
+            alert('O start first')
+            }else{
+            alert('X start first')
+            }
+            
+} 
+
+//startGame Button event
+startButton.addEventListener('click', startGame)
+
+
+
+// Main Function for Game play,  Switch user by switchCounter 1/0,  0 = O, 1 = X
+
+function ox(){
+        
+        // this.childNodes.length prevent repeat entry of O or X, switchCounter for switching 
+        if(this.childNodes.length === 0 && switchCounter === 0){
+
+            // switchCounter = 0 , Need A message div who's turn(I am too lazy for that, you guys guess it who's turn now)
+            //appendChild(spanTag.cloneNode())  you must clone new Node for different parent element
+            let newSpan = this.appendChild(spanTag.cloneNode())
+            // add O class to child Node 
+            newSpan.classList.add("o")
+            // store id to parent to track user 
+            this.dataset.id = switchCounter
+
+                
+            
+            // pushing grid number into an array for Vaildation
+            listofSubBoxes.forEach(function(item){
+
+                    // to validate which number push to the array
+                    // I use id to track user, id default value is reset,
+                    //1 is X player
+                    //As this is O player, it must have id, it must not be 1, it must not be repeated number
+                if(item.dataset.id !== 'reset' && Number(item.dataset.id) !== 1  && counterO.indexOf(Number(item.className)) === -1){
+
+                    let gridNumber = Number(item.className)
+                    counterO.push(gridNumber)
+                    
+                }
+                })
+            
+
+            //winning counter for O
+            winningCombine.forEach( (item) => {
+                // For winner O
+                    let winningCounter = 0
+                    let winningNumber = 0
+                    for(let i = 0; i < counterO.length; i++){
+                            winningNumber = item.indexOf(counterO[i])
+                            if(winningNumber !== -1){
+                                winningCounter++
+                            }
+                    }
+                    //It must be equal 1 set of 3 numbers winning combination 
+                    if(winningCounter === 3){
+                        alert(`O is the winner`)
+
+                        winner = true
+                    //Remove grid event listener
+                    listofSubBoxes.forEach ( 
+                        function(item){item.removeEventListener('click', ox)
+                   
+                        })
+
+                    }
+
+            })
+
+                //draw when draw 
+                if(winner === 0 && gameCounter === 8){
+                    alert(`DRAW`)
+
+                        winner = true
+                    //Remove grid event listener
+                    listofSubBoxes.forEach ( 
+                        function(item){item.removeEventListener('click', ox)
+                   
+                        })
+
+                }
+          
+            switchCounter++
+            gameCounter++
+
+
+        }else if(this.childNodes.length === 0 && switchCounter === 1){
+            // switchCounter = 1 , Need A message div who's turn
+            let newSpan = this.appendChild(spanTag.cloneNode())
+            newSpan.classList.add("x")
+            this.dataset.id = switchCounter
+            
+
+            // print(this.value)
+            // print(this.className)
+            listofSubBoxes.forEach( (item) => print(`${item.dataset.id} ${item.className}  `) )
+
+             // pushing grid number into an array for check
+            listofSubBoxes.forEach(function(item){
+                if(item.dataset.id !== 'reset' && Number(item.dataset.id) !== 0  && counterX.indexOf(Number(item.className)) === -1){
+
+                    let gridNumber = Number(item.className)
+                    counterX.push(gridNumber)
+                }
+                
+            
+                })
+                
+                print(counterX)
+
+                print(`gameCounter ${gameCounter}`)
+
+                //winning counter for O
+            winningCombine.forEach( (item) => {
+                // For winner O
+                    let winningCounter = 0
+                    let winningNumber = 0
+                    for(let i = 0; i < counterX.length; i++){
+                            winningNumber = item.indexOf(counterX[i])
+                            if(winningNumber !== -1){
+                                winningCounter++
+                            }
+                    }
+
+                    if(winningCounter === 3){
+                        alert(`X is the winner`)
+
+                        winner = true
+                        
+                         //Remove grid event listener
+                    listofSubBoxes.forEach ( 
+                        function(item){item.removeEventListener('click', ox)
+                   
+                        })
+                    }
+
+            })
+
+            
+            //draw
+            if(winner === 0 && gameCounter === 8){
+                alert(`DRAW`)
+
+                    winner = true
+                //Remove grid event listener
+                listofSubBoxes.forEach ( 
+                    function(item){item.removeEventListener('click', ox)
+               
+                    })
+
+            }
+                       
+
+            switchCounter--
+            gameCounter++
+            
+        }
+
+}
+
+//for printing too lazy for console.log
+function print(item){ console.log(item)}
+     
